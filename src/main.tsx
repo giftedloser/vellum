62beb6525a95059e3207419b3f159503f1e9e08e
Marshall <128746330+giftedloser@users.noreply.github.com>
2026-07-15 10:32:30 -0600

    Load audited interaction fixes

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import "./refinement.css";
 import "./final.css";
+import "./audit.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
