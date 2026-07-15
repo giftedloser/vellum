bddea840012ddc3dc25904f281871bb1ca7b8006
Marshall <128746330+giftedloser@users.noreply.github.com>
2026-07-15 16:05:04 -0600

    Open associated documents at startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { bootstrapStartupDocument } from "./startup";
 import "./styles.css";
 import "./refinement.css";
 import "./final.css";
 import "./control-system.css";
+
+await bootstrapStartupDocument();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
