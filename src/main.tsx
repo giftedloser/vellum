65113846231c846e6ad95fdec1267267e8e889df
Marshall <128746330+giftedloser@users.noreply.github.com>
2026-07-16 01:03:03 -0600

    Load editor-enabled application

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import VellumApp from "./VellumApp";
 import { bootstrapStartupDocument } from "./startup";
 import "./styles.css";
 import "./refinement.css";
 import "./final.css";
 import "./control-system.css";
+import "./editor.css";
 
 await bootstrapStartupDocument();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <VellumApp />
   </React.StrictMode>,
 );
