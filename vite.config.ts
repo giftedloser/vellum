e33256d813be64c4c9c655211fd3c94b1863ab65
Marshall <128746330+giftedloser@users.noreply.github.com>
2026-07-16 02:03:07 -0600

    Use a build target compatible with startup await

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,5 @@ export default defineConfig({
     watch: { ignored: ["**/src-tauri/**"] },
   },
   envPrefix: ["VITE_", "TAURI_ENV_*"],
-  build: { target: runtime.process?.env?.TAURI_ENV_PLATFORM === "windows" ? "chrome105" : "safari13" },
+  build: { target: runtime.process?.env?.TAURI_ENV_PLATFORM === "windows" ? "chrome105" : "safari15" },
 });
